e5e3d4d45fb7a46407bc7b68c983235782697855
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-27 19:19:46 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :countries, only: [:index, :show]
+      resources :types, only: [:index, :show]
+      resources :categories, only: [:index, :show]
+      resources :themes, only: [:index, :show]
+      resources :category_items, only: [:index, :show]
+    end
+  end
 end
